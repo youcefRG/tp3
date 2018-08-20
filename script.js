805a5c87779185240a2762dd193657493bf4922b
youcef kolli <32937714+youcefRG@users.noreply.github.com>
2018-08-20 11:59:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+function zone_1() {
+
+    document.getElementById('Carte_Oran').src = "map1.PNG";
+}
+function zone_2() {
+   
+    document.getElementById('Carte_Oran').src="map2.PNG";
+}
+function carte_oran() {
+
+    document.getElementById('Carte_Oran').src = "map.jpg";
+}
+function zone_3() {
+
+    document.getElementById('Carte_Oran').src = "map3.PNG";
+}
+function zone_4() {
+
+    document.getElementById('Carte_Oran').src = "map4.PNG";
+}
+function zone_5() {
+
+    document.getElementById('Carte_Oran').src = "map5.PNG";
+}
+function zone_6() {
+
+    document.getElementById('Carte_Oran').src = "map6.PNG";
+}
+function zone_7() {
+
+    document.getElementById('Carte_Oran').src = "map7.PNG";
+}
+function zone_8() {
+
+    document.getElementById('Carte_Oran').src = "map8.PNG";
+} function zone_9() {
+
+    document.getElementById('Carte_Oran').src = "map9.PNG";
+    
+}
+
